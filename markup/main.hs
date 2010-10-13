6cb8694d0cc9f6c4a27b34a83b0eb74f56beb57f
Luis Pedro Coelho <lpc@cmu.edu>
2010-10-13 11:25:47 -0400

    Type declarations and better types

--- markup/main.hs
+++ markup/main.hs
@@ -3,23 +3,23 @@ module Main where
 import Text.ParserCombinators.Parsec hiding (newline,tab)
 import System.Environment
 
---newline :: Parser Char
+newline :: Parser Char
 newline = try (string "\r\n" >> return '\n')
     <|> (string "\r" >> return '\n')
     <|> (string "\n" >> return '\n')
 
---tab :: Parser String
+tab :: Parser String
 tab = (string "\t") >> (return "        ")
 
 
 charToString :: Char -> Parser String
 charToString x = return [x]
 
---line :: Parser String
+line :: Parser String
 line = do
     contents <- many ((letter >>= charToString) <|> (string " ") <|> tab) 
     newline
-    return contents
+    return $ foldl (++) "" contents
 
 getlines = many line
 
