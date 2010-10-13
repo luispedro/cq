1fd0764f92266f333ed8117c4ebbf71e3fd37750
Luis Pedro Coelho <lpc@cmu.edu>
2010-10-13 12:27:36 -0400

    One more test (which fails)

--- markup/Tests.hs
+++ markup/Tests.hs
@@ -12,7 +12,11 @@ testLineIndent0 = TestCase (assertEqual "should be zero" 0 level)
     where
     Right (IndentedLine level _) = (parseJust line "this is a unindented line\n")
 
-tests = TestList [TestLabel "test1" testLineIndent0]
+testLineIndent2 = TestCase (assertEqual "should be 2" 2 level)
+    where
+    Right (IndentedLine level _) = (parseJust line "  this is a 2-indented line\n")
+
+tests = TestList [TestLabel "test0" testLineIndent0, TestLabel "test2" testLineIndent2]
 
 main = runTestTT tests
 
